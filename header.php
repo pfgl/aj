921e68d8b7ee965d823b8b10dd39847cefd95ea1
Jack Machin <jack.machin125@gmail.com>
2017-01-25 17:27:01 +0000

    Issues

--- header.php
+++ header.php
@@ -54,7 +54,7 @@
 
                   <?php wp_nav_menu(array(
                     'container' => false,                           // remove nav container
-                    'container_class' => '',                 // class of container (should you choose to use it)
+                    'container_class' => '',                    // class of container (should you choose to use it)
                     'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
                     'menu_class' => 'nav top-nav cf',               // adding custom nav class
                     'theme_location' => 'main-nav',                 // where it's located in the theme
